83203d75d8d0b0514091a5952f098ce156582cc5
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2021-04-14 22:22:11 +0300

    commit

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -3,12 +3,14 @@ package hooks;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import redmine.Manager.Context;
+import redmine.Manager.Manager;
 
 public class Hooks {
     @After
     public void afterAll(Scenario scenario) {
-        //Context.saveStashToAllure();
+        Context.saveStashToAllure();
         Context.clearStash();
+        Manager.driverQuit();
     }
 
 }
