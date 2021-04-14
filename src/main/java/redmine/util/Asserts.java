2873c654c00c6f28618d3a09edaf2b1783827968
mgrishchenkova <71334332+mgrishchenkova@users.noreply.github.com>
2021-04-14 20:19:24 +0300

    commit

---
+++ src/main/java/redmine/util/Asserts.java
@@ -0,0 +1,41 @@
+package redmine.util;
+
+import io.qameta.allure.Step;
+import org.testng.Assert;
+
+/**
+ * для корректной работы allure-отчета
+ */
+
+public class Asserts {
+
+    @Step("Сравнение переменных на равенство actual: {0}, expected: {1}")
+    public static void assertEquals(Object actual, Object expected) {
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Step("Сравнение переменных на неравенство actual: {0}, not expected: {1}")
+    public static void assertNotEquals(Object actual, Object expected) {
+        Assert.assertNotEquals(actual, expected);
+    }
+
+    @Step("Сравнение Boolean переменной {0}")
+    public static void assertTrue(Boolean actual, String message) {
+        Assert.assertTrue(actual, message);
+    }
+
+    @Step("Сравнение Boolean переменной {0} ")
+    public static void assertFalse(Boolean actual, String message) {
+        Assert.assertFalse(actual, message);
+    }
+
+    @Step("Проверка переменной {0} на NotNull")
+    public static void assertNotNull(Object actual, String message) {
+        Assert.assertNotNull(actual, message);
+    }
+
+    @Step("Проверка переменной {0} на Null")
+    public static void assertNull(Object actual, String message) {
+        Assert.assertNull(actual, message);
+    }
+}
